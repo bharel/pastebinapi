c8d18cd200e1d9997e4700d69a0ba9376a8b1405
Bar Harel <bzvi7919@gmail.com>
2019-09-08 07:07:54 +0300

    down_url added

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     description="An API for pastebin",
     long_description=long_description,
     long_description_content_type='text/markdown',
+    download_url="https://github.com/bharel/pastebinapi/archive/0.1.0.tar.gz",
     author="Bar Harel",
     author_email="bzvi7919+pastebinapi@gmail.com",
     url="https://github.com/bharel/pastebinapi",
